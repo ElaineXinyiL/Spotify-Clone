d0117576311f1d2177ed7975a39f70d28d7f7692
ElaineXinyiL <liuxinyi_sufe@163.com>
2023-07-24 01:28:19 -0400

    automatic play

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
 
 interface MediaItemProps {
   data: Song;
@@ -13,7 +14,7 @@ const MediaItem: React.FC<MediaItemProps> = ({
   data,
   onClick,
 }) => {
-  //const player = usePlayer();
+  const player = usePlayer();
   const imageUrl = useLoadImage(data);
 
   const handleClick = () => {
@@ -21,7 +22,7 @@ const MediaItem: React.FC<MediaItemProps> = ({
       return onClick(data.id);
     }
   
-    //TODO default turn on player
+    return player.setId(data.id);
   };
 
   return ( 
